81c6038e2e7fe85d904640fda0bda73c0f66d5ca
nzsakib507 <nzsakib507@gmail.com>
2025-06-15 20:02:24 +0600

    A4 js dom hero image and logo problem fixed

--- Assignment-4_JS_Dom_Project/src/script.js
+++ Assignment-4_JS_Dom_Project/src/script.js
@@ -1,21 +1,33 @@
 
+// let allMeals = {};
+let allMeals = [];
+
 //  fatching data from the API
 const loadMeals = () => {
-    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
-        .then((res) => res.json())
-        .then((data) => displayMeals(data.meals))
-        .catch((error) => console.log("Fetch error:", error));
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
+    .then((res) => res.json())
+    .then((data) => {
+      // displayMeals(data.meals)
+      allMeals = data.meals;
+      displayMeals(allMeals);
+    })
+    .catch((error) => console.log("Fetch error:", error));
 };
 loadMeals();
 
 const displayMeals = (meals) => {
-    const mealContainer = document.getElementById("mealContainer");
 
-    meals.forEach((meals) => {
-        const mealCard = document.createElement("div");
-        mealCard.className = "w-full shadow-2xl rounded-2xl";
-        // mealContainer.innerHTML = ""; // Clear previous meals
-        mealCard.innerHTML = `
+  console.log(allMeals);
+  // console.log(meals);
+
+  const mealContainer = document.getElementById("mealContainer");
+  mealContainer.innerHTML = "";
+
+  meals.forEach((meals) => {
+    const mealCard = document.createElement("div");
+    mealCard.className = "w-full shadow-2xl rounded-2xl";
+    // mealContainer.innerHTML = ""; // Clear previous meals
+    mealCard.innerHTML = `
                 <div onclick="foodCardClick()" class="w-full shadow-2xl rounded-2xl">
                     <img src=${meals.strMealThumb} alt="image" class="w-full h-64 object-cover rounded-t-2xl">
                     <div class="text-2xl font-semibold py-3 px-4">${meals.strMeal}</div>
@@ -26,31 +38,14 @@ const displayMeals = (meals) => {
                  </div>
         `;
 
-        mealContainer.append(mealCard);
-        console.log(meals);
-    });
+    mealContainer.append(mealCard);
+  });
 }
 
 function foodCardClick() {
-    alert("You clicked a meal!");
+  alert("You clicked a meal!");
 }
 
-//   meals.forEach((meal) => {
-//     mealContainer.innerHTML += `
-//       <div class="card">
-//         <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-//         <h3>${meal.strMeal}</h3>
-//         <p><strong>Category:</strong> ${meal.strCategory}</p>
-//       </div>
-//     `;
-//   });
-
-
-
-
-// function foodCardClick() {
-
-// }
 
 
 
@@ -58,17 +53,46 @@ function foodCardClick() {
 const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
 // Show the button when the user scrolls down 20px from the top of the document
-window.onscroll = function() {
+window.onscroll = function () {
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    scrollToTopBtn.classList.remove ("hidden");
-    scrollToTopBtn.classList.add ("block");
+    scrollToTopBtn.classList.remove("hidden");
+    scrollToTopBtn.classList.add("block");
   } else {
-    scrollToTopBtn.classList.add ("hidden");
-    scrollToTopBtn.classList.remove ("block");
+    scrollToTopBtn.classList.add("hidden");
+    scrollToTopBtn.classList.remove("block");
   }
 };
 
 // Scroll to the top of the document when the button is clicked
-scrollToTopBtn.onclick = function() {
+scrollToTopBtn.onclick = function () {
   window.scrollTo({ top: 0, behavior: 'smooth' });
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //   meals.forEach((meal) => {
+  //     mealContainer.innerHTML += `
+  //       <div class="card">
+  //         <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+  //         <h3>${meal.strMeal}</h3>
+  //         <p><strong>Category:</strong> ${meal.strCategory}</p>
+  //       </div>
+  //     `;
+  //   });
+
+
+
+
+  // function foodCardClick() {
+
+  // }
 };
